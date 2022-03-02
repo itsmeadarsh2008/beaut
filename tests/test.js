84b5909a57e20bb6cb11b8f7c97a3c3535fb2067
Adarsh Gourab Mahalik <71959210+itsmeadarsh2008@users.noreply.github.com>
2022-03-02 17:26:35 +0530

    Add files via upload

---
+++ tests/test.js
@@ -0,0 +1,11 @@
+const beaut = require('..');
+const styler = beaut.beaut;
+
+// Icons Can't Be Visible In Terminals Like Classic CMD Or Powershell
+console.log(beaut.icons.info[0]);
+console.log(beaut.icons.info[1]);
+console.log(beaut.icons.warning)
+console.log(beaut.icons.error)
+console.log(beaut.icons.debug[0])
+console.log(beaut.icons.debug[1])
+console.log(beaut.icons.success)
